471269cd4a2356efbefabf7b6a2d33c095588c48
esc0115 <esc0115@gmail.com>
2020-12-25 12:47:55 +0900

    first code

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import getLocation from './hooks/getLocation';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
@@ -9,7 +10,8 @@ import Navigation from './navigation';
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
-
+  getLocation();
+  
   if (!isLoadingComplete) {
     return null;
   } else {
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,57 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+
+import React, {useState} from 'react';
+import { StyleSheet,Dimensions } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
-
+import MapView,{Marker} from 'react-native-maps';
+interface IGeolocation {
+  latitude: number;
+  longitude: number;
+}
 export default function TabOneScreen() {
+  const [location, setLocation] = useState<IGeolocation>({
+    latitude:  35.56964060014485,
+    longitude: 126.8517660163343,
+  });
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Map MapView</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        onRegionChange={region => {
+          setLocation({
+            latitude: region.latitude,
+            longitude: region.longitude,
+          });
+          console.log(region.latitude,region.longitude)
+        }}
+        onRegionChangeComplete={region => {
+          setLocation({
+            latitude: region.latitude,
+            longitude: region.longitude,
+          });
+        }}>
+        <Marker
+          coordinate={{
+            latitude: 35.56964060014485,
+            longitude: 126.8517660163343,
+          }}
+          title="맥도날드, 정읍"
+          description="현재 내위치"
+        />
+      </MapView>
+
+    </View>
     </View>
   );
 }
@@ -28,5 +70,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
